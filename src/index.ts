1942c89a8d7a1dc29300589b52e0877f28cc5a8d
wolfy1339 <webmaster@wolfy1339.com>
2021-01-10 16:22:26 -0500

    Start implementing the schema editing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import type { Probot, WebhookPayloadWithRepository } from 'probot';
 import type { WebhookEvents } from '@octokit/webhooks';
-import Ajv from 'ajv';
+import Ajv, { ErrorObject } from 'ajv';
 
 import webhooksSchema from '@octokit/webhooks-definitions/schema.json';
+import type { JSONSchema7, JSONSchema7Type } from 'json-schema';
 import type { components } from '@octokit/openapi-types';
 
 type GetRepoContentResponseDataFile = components["schemas"]["content-file"]
+type JSONObject = {
+  [key: string]: JSONObject | string | number | null | boolean | Array<JSONObject>
+}
 
 export default async (app: Probot) => {
   app.on('*', async context => {
@@ -54,7 +58,7 @@ function validateSchema(payload: WebhookPayloadWithRepository, event: WebhookEve
 
   let hasErrors = false;
   try {
-    const validationResult = ajv.validate<typeof webhooksSchema>(webhooksSchema, payload);
+    const validationResult = ajv.validate(webhooksSchema, payload);
     if (!validationResult) {
       console.error(`❌ Payload '${event}${payload.action ?? ''}' does not match schema`);
       console.error(ajv.errors);
@@ -71,7 +75,77 @@ function validateSchema(payload: WebhookPayloadWithRepository, event: WebhookEve
   }
   return { validated: true }
 }
+function editContentToFixSchema(content: string, result: { validated: false; errors: ErrorObject<string, Record<string, any>, JSONSchema7>[] }) {
+  // Sample output
+  /*{
+    "validate": false,
+    "errors": [
+      {
+        "keyword": "type",
+        "dataPath": "/head_commit",
+        "schemaPath": "#/properties/head_commit/type",
+        "params": {
+          "type": "null"
+        },
+        "message": "should be null"
+      },
+      {
+        "keyword": "oneOf",
+        "dataPath": "",
+        "schemaPath": "#/oneOf",
+        "params": {
+          "passingSchemas": null
+        },
+        "message": "should match exactly one schema in oneOf"
+      }
+    ]
+  }*/
+  result.errors.forEach(err => {
+    const [ , ...path ] = err.dataPath.split('/');
+    const [ , ...schemaPath ] = err.schemaPath.split('/') as (keyof JSONSchema7)[];
 
-function editContentToFixSchema(content: string, _result: { validated: boolean; errors: Ajv['errors'] }) {
+    let property: any | undefined;
+    for (let p of path) {
+      let d = err.data as WebhookPayloadWithRepository
+      if (property === undefined) {
+        property = d[p]
+      } else {
+        property = property[p]
+      }
+    }
+    
+    let schemaProperty: any | undefined
+    for (let p of schemaPath) {
+      let d = err.schema!
+      if (schemaProperty === undefined) {
+        schemaProperty = d[p]
+      } else {
+        schemaProperty = schemaProperty[p]
+      }
+    }
+    
+    let sP: JSONSchema7 = schemaProperty!
+    let jP: JSONObject = property
+
+    switch (err.keyword) {
+      case 'type': {
+        if (Array.isArray(sP.oneOf)) {
+          // @ts-expect-error We won't ever have any functions, BigInt or any non-primitive type
+          sP.oneOf.push({ type: typeof jP })
+        } else {
+          // @ts-expect-error We won't ever have any functions, BigInt or any non-primitive type
+          sP.oneOf = [ { type: sP.type, enum: sP.enum }, { type: typeof jP } ]
+        }
+        break;
+      }
+      case 'additionalProperties': {
+        
+        break;
+      }
+      case 'enum': {
+        (sP as JSONSchema7Type[])!.push(jP === null ? 'null' : jP)
+      }
+    }
+  })
   return content
 }
